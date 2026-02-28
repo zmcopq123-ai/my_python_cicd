4559fbe086303d919499e8f2034a092a620b03ac
zmcopq123-ai <zmcopq123@gmail.com>
2026-02-28 14:09:42 +0800

    增加平方功能

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import math
+
+
 def add(a, b):
     """+加法函数"""
     return a + b
@@ -16,9 +19,17 @@ def divide(a, b):
         raise ValueError("除数不能为0")
     return a / b
 
+def square_root(a):
+    """开平方函数"""
+    if a < 0:
+        raise  ValueError("不能对负数开平方")
+    return math.sqrt(a)
+
+
 if __name__ == "__main__":
     print("简单计算器")
     print(f"10 + 5 = {add(10, 5)}")
     print(f"10 - 5 = {subtract(10, 5)}")
     print(f"10 × 5 = {multiply(10, 5)}")
     print(f"10 ÷ 5 = {divide(10, 5)}")
+    print(f"100 的开平方 = {square_root(100)}")
